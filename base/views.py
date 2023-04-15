e44d6e121fb0f263f647d03347faaa2f814e640f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-04-15 22:15:23 +0500

    ✏️ Added send_telegram function

--- base/views.py
+++ base/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from .models import Client, Contact
 from .forms import ContactForm
+import os
+import requests
 
 # Create your views here.
 
+
+def send_telegram(message_data: dict):
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    ADMINS = os.environ.get('ADMINS').split(',')
+    url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
+    message = "Ismi: " + message_data.get('full_name') + "\nEmail: " + \
+        message_data.get('email') + "\nMavzu: " + \
+        message_data.get('subject') + "\n\nXabar: " + \
+        message_data.get('message')
+    for admin in ADMINS:
+        try:
+            requests.post(url, {'chat_id': admin, 'text': message})
+        except Exception:
+            pass
 
 def home_page(request):
     msg = False
@@ -13,14 +29,13 @@ def home_page(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            send_telegram(request.POST)
             msg = True
     clients = Client.objects.all()
     return render(request, 'index.html', {'clients': clients, 'msg': msg})
 
 
 def more_services(request):
-    if request.method == "POST":
-        print('alo asdfaf')
     return render(request, 'more_services.html')
 
 
